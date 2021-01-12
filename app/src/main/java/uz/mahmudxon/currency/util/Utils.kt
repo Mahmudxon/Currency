825c8c99ba05ad488053ea504a2a9b8c3dfe6401
Mahmudxon <mahmudxon@gmail.com>
2021-01-12 16:20:05 +0500

    Delete unused Code

--- app/src/main/java/uz/mahmudxon/currency/util/Utils.kt
+++ app/src/main/java/uz/mahmudxon/currency/util/Utils.kt
@@ -2,12 +2,6 @@ package uz.mahmudxon.currency.util
 
 const val TAG = "TTT"
 
-fun <T : Comparable<T>> ArrayList<T>.insertUniqueItem(item: T) {
-    if (this.contains(item))
-        return
-    this.add(item)
-}
-
 fun <E> ArrayList<E>.insertIfNotExists(item: E) {
     if (this.contains(item))
         return
